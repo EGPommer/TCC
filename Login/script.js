7108d03ee374098f5824465347fa6a5ce1e62157
bszinnnnn <110098564+bszinnnnn@users.noreply.github.com>
2023-08-07 20:55:32 -0300

    Delete Login directory

--- Login/script.js
+++
@@ -1,8 +0,0 @@
-// Código JavaScript opcional para melhorar a compatibilidade com navegadores mais antigos
-(function() {
-    if (window.__disableSmoothScrollPolyfill) return;
-    if ('scrollBehavior' in document.documentElement.style) return;
-    var polyfill = document.createElement('script');
-    polyfill.src = 'https://cdnjs.cloudflare.com/ajax/libs/iamdustan-smoothscroll/0.4.0/smoothscroll.min.js';
-    document.head.appendChild(polyfill);
-})();
